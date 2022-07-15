57e633ecaf02434693990b9c6b3e0d0b14118693
thenist <p717394@gmail.com>
2022-07-15 17:19:46 +0900

    Move overlay-xdroid to overlay

--- device.mk
+++ device.mk
@@ -24,5 +24,4 @@ $(call inherit-product, vendor/samsung/starlte/starlte-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xdroid
+    $(LOCAL_PATH)/overlay
